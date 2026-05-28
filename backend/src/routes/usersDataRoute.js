19302c36d51393bfba98a704699afa03b8d9f1e6
Priyanshu61103 <priyanshuparashar108@gmail.com>
2026-05-29 02:51:57 +0530

    userData Route Testing Done

--- backend/src/routes/usersDataRoute.js
+++ backend/src/routes/usersDataRoute.js
@@ -4,6 +4,6 @@ import { jwtVerifyMiddleware } from "../middlewares/jwtVerifyMiddleware.js";
 
 const usersDataRoute = express.Router();
 
-usersDataRoute.get("/users-data",usersDataController);
+usersDataRoute.get("/users-data",jwtVerifyMiddleware,usersDataController);
 
 export default usersDataRoute;
