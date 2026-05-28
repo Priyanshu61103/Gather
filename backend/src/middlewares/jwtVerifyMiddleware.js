acaff78ec5a9863ee6ac61a396efa8f9742df337
Priyanshu61103 <priyanshuparashar108@gmail.com>
2026-05-29 03:03:28 +0530

    CommitCHange 2

--- backend/src/middlewares/jwtVerifyMiddleware.js
+++ backend/src/middlewares/jwtVerifyMiddleware.js
@@ -3,12 +3,13 @@ import jwt from "jsonwebtoken";
 export function jwtVerifyMiddleware(req, resp , next) {
   try {
     const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-     console.log(req.cookies);
+    console.log(req.cookies);
     const token = req.cookies.token;
     const result = jwt.verify(token, JWT_SECRET_KEY);
     next();
     return;
   } catch (error) {
+     console.log(req);
      console.log(error.message);
      return resp.status(401).send({message:"Unauthorized User",success:false}); 
   }
