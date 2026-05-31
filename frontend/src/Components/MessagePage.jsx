fd8661dce9e25919fff3cfac9fa0d75a3060a89b
Priyanshu61103 <priyanshuparashar108@gmail.com>
2026-05-31 21:49:57 +0530

    message  page fix 10

--- frontend/src/Components/MessagePage.jsx
+++ frontend/src/Components/MessagePage.jsx
@@ -107,7 +107,7 @@ const MessagePage = () => {
 
   return (
     <div>
-      <div className="h-fit flex flex-col lg:ml-[250px] w-[375px] lg:w-screen bg-gray-100">
+      <div className="h-fit flex flex-col lg:ml-[250px] w-[375px] lg:w-[1250px] bg-gray-100">
         <Sidebar />
         <div className="h-fit w-full bg-gray-200 p-2 mr-20 lg:mr-0 mb-12 lg:mb-0 lg:mx-16 px-10 py-1 relative top-16 lg:top-0 border-b-2 border-gray-400">
           <div>
