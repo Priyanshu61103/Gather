7f555569b5979261ecba9af56c57b15d29b0bd40
Priyanshu61103 <priyanshuparashar108@gmail.com>
2026-06-02 00:54:10 +0530

    commit changes 23

--- frontend/src/Components/ConnectionsTab.jsx
+++ frontend/src/Components/ConnectionsTab.jsx
@@ -46,7 +46,7 @@ const ConnectionsTab = () => {
           return getData(itr);
         });
         let arr3 = await Promise.all(arr);
-        console.log(arr3);
+        console.log("Array:",arr3);
         setConnectionArray(arr3);
       } else {
         navigate("/");
