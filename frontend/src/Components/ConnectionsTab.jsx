e62333e714077e4ca752496cccce2883049961aa
Priyanshu61103 <priyanshuparashar108@gmail.com>
2026-06-01 23:47:46 +0530

    commit changes 9

--- frontend/src/Components/ConnectionsTab.jsx
+++ frontend/src/Components/ConnectionsTab.jsx
@@ -11,17 +11,14 @@ const ConnectionsTab = () => {
   const navigate = useNavigate();
   const getData = async (email) => {
     try {
-      const response = await fetch(
-        `/api/profile`,
-        {
-          method: "POST",
-          credentials: "include",
-          body: JSON.stringify({ email }),
-          headers: {
-            "content-type": "application/json",
-          },
+      const response = await fetch(`/api/profile`, {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify({ email }),
+        headers: {
+          "content-type": "application/json",
         },
-      );
+      });
 
       if (!response) return;
 
@@ -61,42 +58,46 @@ const ConnectionsTab = () => {
   }, [userData]);
 
   return (
-    <div className="w-80 h-fit bg-white rounded-lg p-5">
-      <h1 className="font-bold text-black">Connections</h1>
+    <div>
       {connectionArray ? (
-        connectionArray.map((data) => (
-          <Link to={`/profile/${data[0].email}`}>
-            <div
-              className="flex justify-between my-4 hover:bg-neutral-200 rounded-full"
-              key={data[0]._id}
-            >
-              <div className="flex gap-x-2 ">
-                <div>
-                  {data && data[0].profile_picture && (
-                    <img
-                      src={data[0].profile_picture}
-                      alt=""
-                      className="h-12 w-12 rounded-full"
-                    />
-                  )}
+        <div className="w-80 h-fit bg-white rounded-lg p-5">
+          <h1 className="font-bold text-black">Connections</h1>
+          {connectionArray.map((data) => (
+            <Link to={`/profile/${data[0].email}`}>
+              <div
+                className="flex justify-between my-4 hover:bg-neutral-200 rounded-full"
+                key={data[0]._id}
+              >
+                <div className="flex gap-x-2 ">
+                  <div>
+                    {data && data[0].profile_picture && (
+                      <img
+                        src={data[0].profile_picture}
+                        alt=""
+                        className="h-12 w-12 rounded-full"
+                      />
+                    )}
+                  </div>
+                  <div>
+                    {data && data[0].profile_picture && (
+                      <h1 className="font-bold text-sm text-black">
+                        {data[0].full_name}
+                      </h1>
+                    )}
+                    <p className="w-48 text-xs text-gray-600 truncate">
+                      {data[0].bio}
+                    </p>
+                  </div>
                 </div>
-                <div>
-                  {data && data[0].profile_picture && (
-                    <h1 className="font-bold text-sm text-black">
-                      {data[0].full_name}
-                    </h1>
-                  )}
-                  <p className="w-48 text-xs text-gray-600 truncate">
-                    {data[0].bio}
-                  </p>
-                </div>
+                <div></div>
               </div>
-              <div></div>
-            </div>
-          </Link>
-        ))
+            </Link>
+          ))}
+        </div>
       ) : (
-        <div className="font-semibold flex justify-center">No Connections Yet</div>
+        <div className="font-semibold flex justify-center">
+          No Connections Yet
+        </div>
       )}
     </div>
   );
