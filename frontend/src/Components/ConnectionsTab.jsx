aca14072f17ced765d2398382773ac512da19fdb
Priyanshu61103 <priyanshuparashar108@gmail.com>
2026-06-02 00:41:23 +0530

    commit changes 21

--- frontend/src/Components/ConnectionsTab.jsx
+++ frontend/src/Components/ConnectionsTab.jsx
@@ -41,6 +41,7 @@ const ConnectionsTab = () => {
       if (userData != [] && userData.payload && userData.payload != []) {
         let arr = userProfileData.payload[0].connections;
         arr = arr.slice(0,3);
+        console.log(arr);
         let arr2 = arr.map((itr) => {
           return getData(itr);
         });
