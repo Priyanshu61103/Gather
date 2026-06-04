5461ff96ec7b7d39f60e23aaad0e5f745a64f6f6
Priyanshu61103 <priyanshuparashar108@gmail.com>
2026-06-04 22:00:40 +0530

    commir changes 57

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -134,7 +134,7 @@ const Profile = () => {
           <div className=" bg-gray-200">
             <div>
               {alertBtn && (
-                <div className="relative right-8 z-20">
+                <div className="relative top-20 left-8 z-20">
                   <Alert />
                 </div>
               )}
